8b000d1eba3103cac1c2463de7b7395ff40ce4bc
naorh <Hh123456>
2019-05-23 10:55:32 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     ContentComponent
   ],
   imports: [
