cfa30066fb558d779976186001943ad1230c412a
naorh <Hh123456>
2019-05-23 11:30:53 +0300

    camera component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContentComponent } from './content/content.component';
+import { PreviewComponent } from './preview/preview.component';
+import {WebcamModule} from 'ngx-webcam';
+import { CameraComponent } from './camera/camera.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContentComponent
+    ContentComponent,
+    PreviewComponent,
+    CameraComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    WebcamModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
