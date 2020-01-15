366aa31ef831ce1c19510365c0be988af93506fd
Naor Haimov <nhaimov@salesforce.com>
2020-01-15 21:31:39 +0200

    procfile

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -12,7 +12,7 @@ export class ApiService {
 
   getMatch(imageSrc: any): Observable<any> {
     console.log(imageSrc);
-    return this.http.post(`http://holocaustwin.herokuapp.com/getSimilarVictim`, {path: imageSrc})
+    return this.http.post(`https://holocaustwin.herokuapp.com/getSimilarVictim`, {path: imageSrc})
       .pipe(
         map((res) => {
           return (res);
