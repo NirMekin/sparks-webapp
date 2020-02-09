effbf8ecf8a07d299719033441040d10e26bbb44
Naor Haimov <nhaimov@salesforce.com>
2020-02-09 08:35:59 +0200

    empty commit

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -43,7 +43,7 @@ export class PreviewComponent implements OnInit {
           this.noMatch = true;
         }
       },
-        err => {
+        (err) => {
         this.error.emit();
         });
   }
