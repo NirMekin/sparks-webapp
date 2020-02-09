341a27307026832bd8fd664c30a9ec2e3a81bd62
NirMekin <nir.mekin@gmail.com>
2020-02-09 18:45:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/sparks-webapp'));
 
+app.get('/my-twin-favicon.ico', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/sparks-webapp/my-twin-favicon.ico'));
+});
+
 app.get('/*', function(req,res) {
 
   res.sendFile(path.join(__dirname+'/dist/sparks-webapp/index.html'));
